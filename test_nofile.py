b256a42d0b48bed09727b3f1b8261dd63081a1e5
PekeIsBae <crashbug06@gmail.com>
2021-01-03 20:35:20 +0000

    Cleaned up the code, using inheritance and overriding to reduce the spaced used and account for slight variations between similar objects.

--- test_nofile.py
+++ test_nofile.py
@@ -1,4 +1,4 @@
-# Simulate a sokoban level fully with a pre-made level hard-written in code
+# Simulate a Sokoban level fully with a pre-made level hard-written in code
 
 class Game:
 
@@ -22,11 +22,11 @@ class Board:
         self.gtile_coords = [(4, 0), (4, 4)]
         self.player_coords = (2, 2)
         # This will be given as width*height in a text file
-        self.layout = ([Tile(0, None), Tile(1, None), Tile(2, None), Tile(3, None), Tile(4, None)],
-                       [Tile(5, None), Tile(6, None), Tile(7, None), Tile(8, None), Tile(9, None)],
-                       [Tile(10, None), Tile(11, None), Tile(12, None), Tile(13, None), Tile(14, None)],
-                       [Tile(15, None), Tile(16, None), Tile(17, None), Tile(18, None), Tile(19, None)],
-                       [Tile(20, None), Tile(21, None), Tile(22, None), Tile(23, None), Tile(24, None)])
+        self.layout = ([Tile(None), Tile(None), Tile(None), Tile(None), Tile(None)],
+                       [Tile(None), Tile(None), Tile(None), Tile(None), Tile(None)],
+                       [Tile(None), Tile(None), Tile(None), Tile(None), Tile(None)],
+                       [Tile(None), Tile(None), Tile(None), Tile(None), Tile(None)],
+                       [Tile(None), Tile(None), Tile(None), Tile(None), Tile(None)])
         self.p = Player(self.player_coords[0], self.player_coords[1])
         self.place_objs()
 
@@ -37,7 +37,7 @@ class Board:
         for coord in self.box_coords:
             self.layout[coord[0]][coord[1]].change_contents(Box(coord[0], coord[1]))
         for coord in self.gtile_coords:
-            self.layout[coord[0]][coord[1]] = G_Tile(coord[1], None)
+            self.layout[coord[0]][coord[1]] = Goal_Tile(None)
         self.layout[self.player_coords[0]][self.player_coords[1]].change_contents(self.p)
 
     def move_player(self, current, new, direction):
@@ -47,8 +47,6 @@ class Board:
             self.box_to_move = self.layout[new[0]][new[1]].get_contents()
             self.move_box(self.box_to_move.get_curr_pos(), self.box_to_move.get_new_pos(direction), self.box_to_move)
         self.layout[new[0]][new[1]].change_contents(self.p)
-        # Check position by id
-        # print(f'You are at {self.layout[new[0]][new[1]].get_id()}')
 
     def move_box(self, current, new, box):
         self.layout[current[0]][current[1]].change_contents(None)
@@ -61,9 +59,8 @@ class Board:
             for tile in row:
                 if type(tile.get_contents()) is Box:
                     tile.get_contents().update_valid_moves(self.layout)
-                # If all goal tiles contain the box object, then the game is won
-                if type(tile) is G_Tile and type(tile.get_contents()) is not Box:
-                    print(f'Goal at {tile.get_id()} does not have a box')
+                # The game is won if and only if all goal tiles have box on them
+                if type(tile) is Goal_Tile and type(tile.get_contents()) is not Box:
                     self.win = False
         if self.win:
             return True
@@ -76,17 +73,23 @@ class Board:
             elif direction in self.valid_moves:
                 self.move_player(self.p.get_curr_pos(), self.p.get_new_pos(direction), direction)
                 # Check layout after moving
-                for row in self.layout:
-                   print(list(map(lambda x: x.get_contents(), row)))
+                # for row in self.layout:
+                #    print(list(map(lambda x: x.get_contents(), row)))
                 # Check type of tile in layout
                 # for row in self.layout:
                 #    print(row)
                 return False
 
 
-class Player:
+class Moving:
 
-    # startx and starty may become a parameter in a Game_Obj object
+    '''
+    The boxes and player as basically the same object, in terms of how they interact with other objects
+    The only difference between the two is the player can move boxes, but boxes cannot.
+
+    This slight difference is accounted for by overriding the assess_collisions method
+    '''
+
     def __init__(self, starty, startx):
         self.posy = starty
         self.posx = startx
@@ -113,40 +116,61 @@ class Player:
                 if self.new_posx >= 0 and self.new_posy >= 0:
                     self.tile_at_new_pos = board[self.new_posy][self.new_posx]
                     self.item = self.tile_at_new_pos.get_contents()
-                    if type(self.item) is Wall:
-                        #print(f'Wall at {dir}')
-                        self.valid = False
-                    elif type(self.item) is G_Tile:
-                        #print(f'Goal tile at {dir}')
-                        self.valid = False
-                    elif type(self.item) is Box:
-                        #print(f'Box at {dir}')
-                        if dir not in self.item.get_valid_moves():
-                            self.valid = False
+                    self.valid = self.assess_collision(self.item, dir)
                 else:
                     self.valid = False
             except IndexError:
                 self.valid = False
             if self.valid:
                 self.valid_moves.append(dir)
-        # Check if valid directions are calculated correctly
-        # print(self.valid_moves)
         return self.valid_moves
+
+    def assess_collision(self, item, direction):
+        return None
 
     def update_valid_moves(self, board):
         self.valid_moves = self.find_valid_moves(board)
 
     def get_valid_moves(self):
-        # Check box valid moves
-        # print(f'Box can move in {self.valid_moves}')
         return self.valid_moves
+
+
+class Player(Moving):
+
+    def __init__(self, starty, startx):
+        super().__init__(starty, startx)
+
+    def assess_collision(self, item, direction):
+        # Player cannot move if a wall is in the way
+        if type(item) is Wall:
+            return False
+        # If a box is in the way, check if the player is able to push it
+        elif type(item) is Box:
+            if direction not in item.get_valid_moves():
+                return False
+        return True
+
+
+class Box(Moving):
+
+    def __init__(self, starty, startx):
+        super().__init__(starty, startx)
+
+    def assess_collision(self, item, direction):
+        # Boxes cannot move if anything is in their way
+        if item is not None:
+            return False
+        return True
 
 
 class Tile:
 
-    def __init__(self, _id, contents):
-        # To remove
-        self._id = _id
+    '''
+    The regular and goal tile are exactly the same.
+    The goal tile needs to be named differently so the board can assess it differently to normal tiles.
+    '''
+
+    def __init__(self, contents):
         self.contents = contents
 
     def get_contents(self):
@@ -155,78 +179,16 @@ class Tile:
     def change_contents(self, item):
         self.contents = item
 
-    # To remove
-    def get_id(self):
-        return self._id
 
+class Goal_Tile(Tile):
 
-class Game_Obj():
-    pass
+    def __init__(self, contents):
+        super().__init__(contents)
 
-
-class G_Tile(Tile):
-
-    def __init__(self, _id, contents):
-        super().__init__(_id, contents)
-
-class Box:
-
-    def __init__(self, starty, startx):
-        self.posy = starty
-        self.posx = startx
-        self.dirs = ('u', 'd', 'l', 'r')
-        self.movement_vectors = ([-1, 0], [1, 0], [0, -1], [0, 1])
-        self.valid_moves = []
-
-    def get_curr_pos(self):
-        return self.posy, self.posx
-
-    def get_new_pos(self, direction):
-        self.posy += self.movement_vectors[self.dirs.index(direction)][0]
-        self.posx += self.movement_vectors[self.dirs.index(direction)][1]
-        return self.posy, self.posx
-
-    def find_valid_moves(self, board):
-        self.valid_moves = []
-        for counter, dir in enumerate(self.dirs):
-            self.valid = True
-            self.new_posy = self.posy + self.movement_vectors[counter][0]
-            self.new_posx = self.posx + self.movement_vectors[counter][1]
-            try:
-                if self.new_posx >= 0 and self.new_posy >= 0:
-                    self.tile_at_new_pos = board[self.new_posy][self.new_posx]
-                    if self.tile_at_new_pos.get_contents() is not None:
-                        self.item = self.tile_at_new_pos.get_contents()
-                        if type(self.item) is Wall:
-                            #print(f'Box has Wall at {dir}')
-                            pass
-                        if type(self.item) is Box:
-                            #print(f'Box has Box at {dir}')
-                            pass
-                        if type(self.item) is G_Tile:
-                            #print(f'Box has Goal tile at {dir}')
-                            pass
-                        self.valid = False
-                else:
-                    self.valid = False
-            except IndexError:
-                self.valid = False
-            if self.valid:
-                self.valid_moves.append(dir)
-        # Check if valid directions are calculated correctly
-        # print(self.valid_dirs)
-        return self.valid_moves
-
-    def update_valid_moves(self, board):
-        self.valid_moves = self.find_valid_moves(board)
-
-    def get_valid_moves(self):
-        # Check box valid moves
-        # print(f'Box can move in {self.valid_moves}')
-        return self.valid_moves
 
 class Wall:
     pass
+
 
 if __name__ == '__main__':
     g = Game()
