13a2a6db70ae8aa5f9fbad377c69b2e8fbccc100
PekeIsBae <crashbug06@gmail.com>
2021-05-08 19:31:13 +0100

    Workspace area now holds empty slot objects that allow the user to change their contents. Added the first sprite, the wall.png.
    Plan to make the rest of the sprites and change usage of 'fill' to sprite images where necessary.

--- create_level.py
+++ create_level.py
@@ -2,6 +2,7 @@
 # On quitting the level should be written in text json form to the levels folder
 
 import pygame as pg
+import math
 
 FPS = 30
 
@@ -11,10 +12,12 @@ WALL_CLR = (72, 72, 72)  # http://ezavada.com/pdg/javascript/html/classpdg_1_1_c
 TILE_CLR = (250, 218, 94)  # https://graf1x.com/shades-of-yellow-color-palette-chart/ (Royal)
 GTILE_CLR = (93, 187, 99)  # https://www.color-meanings.com/shades-of-green-color-names-html-hex-rgb-codes/ (Fern)
 
+WHITE = (255, 255, 255)
+
 edit_cnfg = {
     'title': 'Create/Edit level',
-    'x': 1100,
-    'y': 700,
+    'w': 1100,
+    'h': 700,
     'bg_col': (136, 136, 136),
     'b_x': 1100/10,
     'b_y': 700/5,
@@ -23,33 +26,43 @@ edit_cnfg = {
     'wrk_x': 1100/2,
     'wrk_y': 700/2,
     'wrk_sizex': 700-50,
-    'wrk_sizey': 700-50
+    'wrk_sizey': 700-50,
+    'base_grid_dim': 4,
 }
 
 
 class GameSprite(pg.sprite.Sprite):
 
-    def __init__(self, x, y, w, h, fill=None):
+    def __init__(self, x, y, w, h):
         # Change fill=None to sprite=None after prototype is working
         super().__init__()
         self.image = pg.Surface((w, h))
-        if fill:
-            self.image.fill(fill)
-
-        '''
-        if sprite is None:
-            self.image = pg.Surface((w, h))
-        else:
-            self.image = pg.image.load(f'images/{sprite}').convert()
-        '''
 
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+
+        # Changing rect.centre only moves the rect's centre to a position, not change what its 'centre' is
+        # So, this is used to make initial sprite placement easier
         self.rect.center = (x, y)
 
 
+class EditButton(GameSprite):
+
+    def __init__(self, x, y, w, h, paint, sprite=None):
+        super().__init__(x, y, w, h)
+        self.paint = paint
+        self.sprite = sprite
+        if self.sprite:
+            self.image = pg.image.load(f'./images/{self.sprite}').convert()
+            self.image = pg.transform.scale(self.image, (w, h))
+
+    def get_paint(self):
+        return self.paint
+
+
 class EmptySlot(GameSprite):
+
     '''
     Empty slots and tiles are fitted onto the grid by a tile buffer
     Each tile and empty slot has a row and column, this multiplied by tile buffer gives pixel pos
@@ -57,64 +70,123 @@ class EmptySlot(GameSprite):
         - Adjust tile buffer appropriately
         - Give all existing tiles and empty slots row and column + 1
     '''
-    pass
+
+    def __init__(self, x, y, w, h, row, col, edge):
+        super().__init__(x, y, w, h)
+        self.rect.center = (x + w/2, y + h/2)
+        self.edge = edge
+        self.contents = None
+        self.row = row
+        self.col = col
+        self.w = w
+        self.h = h
+        if self.edge:
+            self.image.fill((0, 255, 0))
+
+    def get_coords(self):
+        return self.row, self.col
+
+    def get_contents(self):
+        return self.contents
+
+    def update_contents(self, item):
+        self.contents = item
+        self.image = pg.transform.scale(pg.image.load(f'./images/wall.png').convert(), (self.w, self.h))
 
 
 class Workspace(GameSprite):
+
     '''
     How editing will work:
-    - Grid starts as a 9/9
+    - Grid starts as a 4/4
     - Placing an item on the outermost rim of the grid adds an outer layer to the grid, zooming out
     '''
-    pass
+
+    def __init__(self, x, y, w, h, level_data):
+        super().__init__(x, y, w, h)
+        self.empty_slots_group = pg.sprite.Group()
+        self.set_board(edit_cnfg['base_grid_dim'])
+
+    def set_board(self, grid_dim):
+        for row in range(0, grid_dim):
+            for col in range(0, grid_dim):
+                self.edge = False
+                if col == 0 or col == grid_dim - 1 or row == 0 or row == grid_dim - 1:
+                    self.edge = True
+                self.empty_slot = EmptySlot(edit_cnfg['wrk_sizex']/grid_dim*col, edit_cnfg['wrk_sizey']/grid_dim*row,
+                                            math.ceil(edit_cnfg['wrk_sizex']/grid_dim),
+                                            math.ceil(edit_cnfg['wrk_sizey']/grid_dim),
+                                            row, col, self.edge)
+                self.empty_slots_group.add(self.empty_slot)
+        self.empty_slots_group.draw(self.image)
+
+    def localise_pos(self, pos):
+        return pos[0] - self.rect.x, pos[1] - self.rect.y
+
+    def remove_current_player(self):
+        for slot in self.empty_slots_group:
+            if slot.get_contents() == 'player':
+                slot.update_contents(None)
+
+    def events(self, mouse_point, paint):
+        if paint == 'player':
+            self.remove_current_player()
+        for slot in self.empty_slots_group:
+            if slot.rect.collidepoint(self.localise_pos(mouse_point)):
+                slot.update_contents(paint)
+
+        self.empty_slots_group.draw(self.image)
 
 
 class Editor:
 
-    def __init__(self):
+    def __init__(self, level_data):
         pg.init()
-        self.screen = pg.display.set_mode((edit_cnfg['x'], edit_cnfg['y']))
+        self.screen = pg.display.set_mode((edit_cnfg['w'], edit_cnfg['h']))
         self.screen.fill(edit_cnfg['bg_col'])
         self.clock = pg.time.Clock()
         pg.display.set_caption(edit_cnfg['title'])
+        self.level_data = level_data
         self.done = False
+
+        self.selected = None
 
         self.create_widgets()
 
         self.mainloop()
 
     def create_widgets(self):
-        self.player_b = GameSprite(edit_cnfg['b_x'], edit_cnfg['b_y'], edit_cnfg['b_sizex'],
-                                   edit_cnfg['b_sizey'], PLAYER_CLR)
-        self.box_b = GameSprite(edit_cnfg['b_x'], edit_cnfg['b_y']*2, edit_cnfg['b_sizex'],
-                                edit_cnfg['b_sizey'], BOX_CLR)
-        self.wall_b = GameSprite(edit_cnfg['b_x'], edit_cnfg['b_y']*3, edit_cnfg['b_sizex'],
-                                 edit_cnfg['b_sizey'], WALL_CLR)
-        self.gtile_b = GameSprite(edit_cnfg['b_x'], edit_cnfg['b_y']*4, edit_cnfg['b_sizex'],
-                                  edit_cnfg['b_sizey'], GTILE_CLR)
+        self.player_b = EditButton(edit_cnfg['b_x'], edit_cnfg['b_y'], edit_cnfg['b_sizex'],
+                                   edit_cnfg['b_sizey'], 'player')
+        self.box_b = EditButton(edit_cnfg['b_x'], edit_cnfg['b_y']*2, edit_cnfg['b_sizex'],
+                                edit_cnfg['b_sizey'], 'box')
+        self.wall_b = EditButton(edit_cnfg['b_x'], edit_cnfg['b_y']*3, edit_cnfg['b_sizex'],
+                                 edit_cnfg['b_sizey'], 'wall', 'wall.png')
+        self.gtile_b = EditButton(edit_cnfg['b_x'], edit_cnfg['b_y']*4, edit_cnfg['b_sizex'],
+                                  edit_cnfg['b_sizey'], 'gtile')
 
-        self.eraser_b = GameSprite(edit_cnfg['x'] - edit_cnfg['b_x'], edit_cnfg['b_y'], edit_cnfg['b_sizex'],
-                                   edit_cnfg['b_sizey'])
-        self.wipe_b = GameSprite(edit_cnfg['x'] - edit_cnfg['b_x'], edit_cnfg['b_y']*2, edit_cnfg['b_sizex'],
+        self.eraser_b = EditButton(edit_cnfg['w'] - edit_cnfg['b_x'], edit_cnfg['b_y'], edit_cnfg['b_sizex'],
+                                   edit_cnfg['b_sizey'], None)
+        self.wipe_b = GameSprite(edit_cnfg['w'] - edit_cnfg['b_x'], edit_cnfg['b_y']*2, edit_cnfg['b_sizex'],
                                  edit_cnfg['b_sizey'])
-        self.save_b = GameSprite(edit_cnfg['x'] - edit_cnfg['b_x'], edit_cnfg['b_y']*3, edit_cnfg['b_sizex'],
+        self.save_b = GameSprite(edit_cnfg['w'] - edit_cnfg['b_x'], edit_cnfg['b_y']*3, edit_cnfg['b_sizex'],
                                  edit_cnfg['b_sizey'])
-        self.quit_b = GameSprite(edit_cnfg['x'] - edit_cnfg['b_x'], edit_cnfg['b_y']*4, edit_cnfg['b_sizex'],
+        self.quit_b = GameSprite(edit_cnfg['w'] - edit_cnfg['b_x'], edit_cnfg['b_y']*4, edit_cnfg['b_sizex'],
                                  edit_cnfg['b_sizey'])
 
         self.workspace = Workspace(edit_cnfg['wrk_x'], edit_cnfg['wrk_y'],
-                                   edit_cnfg['wrk_sizex'], edit_cnfg['wrk_sizey'])
+                                   edit_cnfg['wrk_sizex'], edit_cnfg['wrk_sizey'], self.level_data)
 
-        self.buttons_group = pg.sprite.Group()
-        self.buttons_group.add(self.player_b, self.box_b, self.wall_b, self.gtile_b,
-                               self.eraser_b, self.wipe_b, self.save_b, self.quit_b)
+        self.palette_group = pg.sprite.Group()
+        self.palette_group.add(self.player_b, self.box_b, self.wall_b, self.gtile_b,
+                               self.eraser_b)
 
         self.workspace_group = pg.sprite.Group()
         self.workspace_group.add(self.workspace)
 
     def draw_groups(self):
         self.workspace_group.draw(self.screen)
-        self.buttons_group.draw(self.screen)
+        self.palette_group.draw(self.screen)
 
     def mainloop(self):
 
@@ -125,6 +197,14 @@ class Editor:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     self.done = True
+                if event.type == pg.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        self.mouse_point = event.pos
+                        for button in self.palette_group:
+                            if button.rect.collidepoint(self.mouse_point):
+                                self.selected = button.get_paint()
+                        if self.workspace.rect.collidepoint(self.mouse_point) and self.selected:
+                            self.workspace.events(self.mouse_point, self.selected)
 
             self.draw_groups()
 
@@ -134,4 +214,4 @@ class Editor:
 
 
 if __name__ == '__main__':
-    win = Editor()
+    win = Editor(None)
